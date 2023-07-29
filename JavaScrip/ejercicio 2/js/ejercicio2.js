520972b026422554ee4b0e6bdb4342da6e874542
JOSEPHELIZALDEVAQUIRO46 <joseph_elizalde@soy.sena.edu.co>
2023-07-28 21:09:33 -0500

    Se hizo el ultimo ejercicio y se pospone el 10 para condicionales

--- JavaScrip/ejercicio 2/js/ejercicio2.js
+++ JavaScrip/ejercicio 2/js/ejercicio2.js
@@ -105,3 +105,17 @@ let rest = parseInt(presupuesto*trau);
 let resp = parseInt(presupuesto*pedi);
 
 alert("el presupuesto del hopital queda dividido de la siguiente manera: $" +resg+ " en el area de ginecología,$" +rest+ " en el area de traumatología y $" +resp+ " en el area de pediatría.");
+
+/*Ejercicio 9 - Tres personas deciden invertir su dinero para fundar una empresa. Cada una de
+ellas invierte una cantidad distinta. Obtener el porcentaje que cada quien invierte
+con respecto a la cantidad total invertida. */
+
+let pers1 = prompt("Ingrese la cantidad de dinero invertido por la primera persona");
+let pers2 = prompt("Ingrese la cantidad de dinero invertido por la segunda persona");
+let pers3 = prompt("Ingrese la cantidad de dinero invertido por la tercera persona");
+let totalp = parseInt(pers1)+ parseInt(pers2)+ parseInt(pers3);
+let tper1 = parseInt(pers1)* parseInt(100/totalp);
+let tper2 = parseInt(pers2)* parseInt(100/totalp);
+let tper3 = parseInt(pers3)* parseInt(100/totalp);
+
+alert("El porcentaje que cada persona invirtio en la fundacion de la empresa es de " +tper1+ "% la primera persona, " +tper2+ "% la segunda persona y " +tper3+ "% la tercera persona.");
